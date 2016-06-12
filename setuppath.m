2293d7b95a7eddb02189fb7e815f51778ad76e2b
ahbarnett <ahb@math.dartmouth.edu>
2016-06-12 16:35:01 -0400

    setuppath

---
+++ setuppath.m
@@ -0,0 +1,9 @@
+function setuppath
+% SETUPPATH   puts all M-files from BIE2D package in path as absolute paths
+%
+% Barnett 6/12/16
+mfilepath=fileparts(mfilename('fullpath'));
+addpath([mfilepath, '/kernels']);
+addpath([mfilepath, '/utils']);
+addpath([mfilepath, '/test']);
+addpath([mfilepath, '/doublyperiodic']);
