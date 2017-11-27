9d23b3f270baf63306d200704d3dab7c1da3aa24
ahbarnett <ahb@math.dartmouth.edu>
2017-11-27 14:35:45 -0500

    perispecinterp doc

--- utils/perispecinterp.m
+++ utils/perispecinterp.m
@@ -1,21 +1,21 @@
 function g = perispecinterp(f,N)
-% PERISPECINTERP   resample periodically sampled function on finer uniform grid.
+% PERISPECINTERP  resample periodically sampled function onto different grid.
 %
 % g = perispecinterp(f,N)
-% inputs:  f - (row or column) vector length n (must be even) of samples
-%          N - desired output number of samples, must be >= n and even
-% outputs: g - vector length N of interpolant, (row or col as f was)
+% inputs:  f - (row or column) vector length n (must be even) of input samples
+%          N - desired output number of samples, must be even
+% outputs: g - vector length N of interpolant (row or col, same size as f)
 %
-% Note on phasing: the output and input grid first entry align (ie, as if they
-%  are both 0-indexed; note this matches setupquad)
+% Note on phasing: the output and input grid first entries align (ie, as if they
+%  are both 0-indexed; note this matches setupquad).
 
-% Barnett 6/27/16 renaming fftinterp from 9/5/14
-% todo: * downsample case N<n.  * odd cases.
+% Barnett 6/27/16 renaming fftinterp from 9/5/14. Upsample case 11/16/17.
+% todo: odd cases.
 
 if nargin==0, test_perispecinterp; return; end
 n = numel(f);
 if N==n, g = f; return; end   % trivial case!
-if mod(N,2)~=0 || mod(n,2)~=0, warning('N and n must be even; sorry'); end
+if mod(N,2)~=0 || mod(n,2)~=0, warning('Both N and n must be even; sorry'); end
 F = fft(f(:).');    % row vector
 if N>n              % upsample
   g = ifft([F(1:n/2) F(n/2+1)/2 zeros(1,N-n-1) F(n/2+1)/2 F(n/2+2:end)]);
@@ -30,7 +30,7 @@ function test_perispecinterp
 n = 100;          % starting grid
 x = 2*pi*(0:n-1)/n;
 f = @(x) exp(sin(x));
-Ns = [142 42];    % upsample then downsample test
+Ns = [142 42];    % upsample test then downsample test
 for i=1:2, N=Ns(i);
   g = perispecinterp(f(x),N);
   ge = f(2*pi*(0:N-1)/N);
