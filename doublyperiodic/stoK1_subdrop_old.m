b9e5ac127b21f7fe2cd5ee97c35c00dd5445cebb
ahbarnett <ahb@math.dartmouth.edu>
2018-02-28 17:19:17 -0500

    trying stoK1_subdrop.m w/ projectors and rank-3 trick in gmres

--- doublyperiodic/stoK1_subdrop_old.m
+++ doublyperiodic/stoK1_subdrop_old.m
@@ -31,7 +31,7 @@ N = 150; s = wormcurve(a,b,N); s.a = mean(s.x);  % a needed for p ext close
 %s.x = s.x + 0.1i;  % check translational invariance of flux
 
 PC = [zeros(2*m,1);jumps(1)+0*U.L.x;zeros(4*m,1);jumps(2)+0*U.B.x]; % physical
-rhs = [zeros(2*N,1); PC]; % driving for ELS
+rhs = [zeros(2*N,1); PC]; % driving
 [E,A,B,C,Q] = ELSmatrix(s,p,proxyrep,mu,sd,U);                % fill blocks
 %figure; imagesc(E); colorbar; drawnow
 if 0   % direct ELS solve (no scatt split), checks the setup is good.
@@ -61,14 +61,13 @@ fixv = 0;   % if true, make v at 1st node vanish, try to exploit in w iter solve
 % solve for v via correction to const sigma: Stokes in U\Omega, has inhomog PC
 perim = sum(s.w);
 constsig = -kron(jumps',ones(N,1)) / perim / sd(1);  % gives left force = jumps
-PC2 = PC - C*constsig;        % PC now consistent
-if ~fixv, xi = linsolve(Q,PC2,lso);
+PC = PC - C*constsig;        % PC now consistent
+if ~fixv, xi = linsolve(Q,PC,lso);
 else, vc = A([1,1+N],:)*constsig;   % velocity at 1st node to cancel w/ aux
-    xi = linsolve([Q;B(1,:);B(1+N,:)],[PC2;-vc(1);-vc(2)],lso); end  % extra cond
-fprintf('v solve: |xi|=%.3g, resid nrm = %.3g\n',norm(xi), norm(Q*xi-PC2))
+    xi = linsolve([Q;B(1,:);B(1+N,:)],[PC;-vc(1);-vc(2)],lso); end  % extra cond
+fprintf('v solve: |xi|=%.3g, resid nrm = %.3g\n',norm(xi), norm(Q*xi-PC))
 vdata = A*constsig + B*xi;   % v on bdry (exterior limit), rep in 2 basis types
 if fixv, disp('vdata at 1st node'); disp(vdata([1,1+N])), end
-fprintf('check v rep matches PC: %.3g\n',norm(C*constsig + Q*xi - PC))
 
 % solve for w
 wBC = -vdata;       % RHS for the density-only lin sys
@@ -77,28 +76,19 @@ wBC = -vdata;       % RHS for the density-only lin sys
 %co = randn(2*N,1); [y,wxi] = mockperifmm(co,A,B,C,Q,s,p); stop % test mock fmm
 %for i=1:3, norm(mockperifmm(H(:,i),A,B,C,Q,s,p)), end, stop  % check zero
 
-til = 0;      % 0: don't perturb B,Q; 1 use Btilde,Qtilde pert via HR'
-
-% test perifmm nullspace is H - it is
-for i=1:3, norm(mockperifmm(H(:,i),A,B,C,Q,s,p,til,fixv)), end
-%inv(Y'*H)   % exists! so Y has full rank proj onto H=Nul(Aper)
-%fprintf('is b in ran(Aper)?');
-
+til = 1;      % 0: don't perturb B,Q; 1 use Btilde,Qtilde pert via HR'
 [Aper genxi] = fillAper(A,B,C,Q,s,p,U,til,fixv); % get full Aper matrix
-if 0       % direct
+if 1       % direct
   S = svd(Aper); disp('last few sing vals of Aper:'); disp(S(end-5:end))
   wsig = linsolve(Aper,wBC,lso);       % direct solve
   fprintf('direct: ||wsig||=%.3g, resid=%.3g\n',norm(wsig),norm(Aper*wsig-wBC))
   wxi = genxi*wsig;  % create xi aux rep for this density soln
   if til, wsig = wsig + H*(R'*wxi); end    % correct the density
   %wsig = wsig - H*((Y'*H)\(Y'*wsig));       % proj orthog to Y - fails
-elseif 1   % ...or, iter  *** NEW ONES MAT
-  rng(0); Z = randn(2*N,3);
-  matvec = @(x) mockperifmm(x,A,B,C,Q,s,p,til,fixv) + Z*(Y'*x);  % 1smat lin sys
+elseif 1   % ...or, iter
+  matvec = @(x) mockperifmm(x,A,B,C,Q,s,p,til,fixv);
   [wsig,flag,relres,iter,resvec] = gmres(matvec,wBC,[],1e-14,2*N);
   its=iter(2),resvec
-  fprintf('pert resid nrm = %.3g\n',norm(matvec(wsig) - wBC))
-  fprintf('orig resid nrm = %.3g\n',norm(mockperifmm(wsig,A,B,C,Q,s,p,til,fixv) - wBC))
   % since lost it, re-grab wxi the aux coeffs for soln dens wsig...
   [~,wxi] = mockperifmm(wsig,A,B,C,Q,s,p,til,fixv);
   if til, wsig = wsig + H*(R'*wxi); end % correct the density
@@ -108,7 +98,6 @@ else       % ...or, ELS solve for just w part...  works, of course
   wsig = co(1:2*N); wxi = co(2*N+1:end);
 end
 disp('Y''*wsig (should all be zero):'); disp(Y'*wsig)
-disp('H''*wsig :'); disp(H'*wsig)
 %disp('WT.C.wsig'); disp(W'*C*wsig);
 %wxi = -Q\(C*wsig);
 %disp('||Q.wxi+C.wsig||'); norm(Q*wxi+C*wsig)
@@ -227,20 +216,17 @@ H = [ones(1,N) zeros(1,N);zeros(1,N) ones(1,N);real(s.nx)' imag(s.nx)']';
 % R is 2M*3, is proxy
 R = [[ones(M,1);zeros(M,1)],[zeros(M,1);ones(M,1)],[real(p.x);imag(p.x)]]/M;
 
-xt=x; al = -(Y'*H)\(Y'*x); xt = x + H*al;    % project x to xt, so Y'*xt=0_3
+xt=x; %al = -(Y'*H)\(Y'*x); xt = x + H*al;    % project x to xt, so Y'*xt=0_3
 %al = -(Y'*Y)\(Y'*x); xt = x + Y*al;   % orthog proj, fails
 g = -C*xt;  % eval the discrep to kill
 %fprintf('mockperifmm: |g|_2 = %.3g\n',norm(g))
 Qtilde = Q; if til, Qtilde = Q + (C*H)*R'; end
 Btilde = B; if til, Btilde = B + (A*H)*R'; end
 if ~fixv, xi = Qtilde\g;   % find aux coeffs to match it. %bare Q amplifies incompat err by 1e9!
-  %fprintf('\tnorm xi = %.3g\n',norm(xi))
-  %fprintf('\t norm (Q R) = %.3g\n',norm(Q*R))
 else, vc = A([1,1+N],:)*xt;   % velocity at 1st node to cancel w/ aux
   xi = [Qtilde;Btilde(1,:);Btilde(1+N,:)] \ [g;-vc(1);-vc(2)]; end  % extra cond
   
-%al = -(R'*R)\(R'*xi); xi = xi + R*al;    % orthog project xi so R'*xt=0_
-%norm(Q*xi - g)  % check is zero, yes
+%al = -(R'*R)\(R'*xi); xi = xi + R*al;    % project xi so R'*xt=0_3
 
 %fprintf('mockperifmm: |xt|_2 = %.3g,  |xi|_2 = %.3g,  |B.xi|=%.3g\n',norm(xt),norm(xi), norm(B*xi))
 y = A*xt + Btilde*xi;   % "3x3 nr FMM + aux eval". note xi=-"X" in matrix case
